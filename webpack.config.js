0b51dd750aa5204f379161bc3bba2916c01b13e1
Shintaro Tanaka <qpshinqp@mist-t.co.jp>
2017-05-30 16:34:16 +0900

    Finefixed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
-/* global require, module */
+/* global require, module, __dirname */
 const webpack = require("webpack");
+const path    = require("path");
 
 module.exports = {
   entry: "./index.js",
   output: {
-    path: "./dist",
+    path: path.resolve(__dirname, "dist"),
     filename: "customrangeinput.js",
   },
   module: {
